b069f1a6b1c0661df39577ea357afc907342c3cd
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-08-29 17:49:49 +0200

    fix: missing return statement

--- popup.js
+++ popup.js
@@ -3,8 +3,10 @@
 		let tmp = await browser.runtime.sendMessage("ping");
 		const url = new URL(tmp);
 		const domain = url.hostname;
+		console.log(domain);
 		if(typeof domain === 'string' && domain !== null && domain !== '') { 
 			document.getElementById('myframe').src = "https://www.alexa.com/minisiteinfo/" + domain;
+			return;
 		}
 	}catch(e) {
 		console.error(e.toString());
