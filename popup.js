22544dcc6ac3888cbe750a9c6b6f91d60c8a6d0f
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-09-10 21:39:57 +0200

    fix: infinit load / no error message with moz-extension urls

--- popup.js
+++ popup.js
@@ -1,16 +1,17 @@
 (async function init() {
 	try {
-		let tmp = await browser.runtime.sendMessage("ping");
-		const url = new URL(tmp);
-		const domain = url.hostname;
-		//console.log(domain);
-		if(typeof domain === 'string' && domain !== null && domain !== '') { 
-			document.getElementById('myframe').src = "https://www.alexa.com/minisiteinfo/" + domain;
-			return;
+		const tmp = await browser.runtime.sendMessage("ping");
+		if(/^https?:/.test(tmp)){
+			const url = new URL(tmp);
+			const domain = url.hostname;
+			if(typeof domain === 'string' && domain !== null && domain !== '') { 
+				document.getElementById('myframe').src = "https://www.alexa.com/minisiteinfo/" + domain;
+				return;
+			}
 		}
 	}catch(e) {
 		console.error(e.toString());
 		//myframe.remove();
 	}
-	document.body.innerHTML = '<p> tab url has no valid domain name <p>';
+	document.body.innerHTML = '<p> tab url has no valid domain or protocol<p>';
 }());
